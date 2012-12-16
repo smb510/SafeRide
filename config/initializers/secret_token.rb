5a1d09b2a16c5cc2238787ef390f3a2d2dc7c79a
Scottie Biddle <smb510@gmail.com>
2012-12-16 14:38:07 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SafeRide::Application.config.secret_token = '84e485c11b9ccda3b3e924847b321b15baeb51f2aa216bf2ad0d8b9804b184e57532676da2725951093af0babc47f1a5433fce4291f98657bc4be7ac37ded9dd'
